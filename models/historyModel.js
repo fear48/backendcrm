10b94c1ab06739f22c96031395816353b16d08e4
Ilya Kulgavy <i.kulgavy@gmail.com>
2017-08-29 12:25:35 +0300

    History`s field added

--- models/historyModel.js
+++ models/historyModel.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema;
 const HistoryModel = new Schema({
   uid: { type: String, required: true },
   date: { type: String, required: true },
+  time: { type: String, required: true },
   room: { type: String, required: true }
 });
 
