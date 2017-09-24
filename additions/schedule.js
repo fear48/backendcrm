10c551976866108200d5b764e8da59d52c7a5662
Ilya Kulgavy <i.kulgavy@gmail.com>
2017-09-24 16:01:18 +0300

    Fixes about config

--- additions/schedule.js
+++ additions/schedule.js
@@ -3,15 +3,17 @@ import Agenda from "agenda";
 import config from '../config/config'
 import Event from '../models/eventModel'
 
-const { values: { paymentExpiresAt }, database } = config;
+const { database } = config;
 const agenda = new Agenda({ db: { address: database } })
 
 
 agenda.define('delete old events', (job, done) => {
+  const { values: { paymentExpiresAt } } = config;
   const date = new Date();
   date.setHours(date.getHours() - paymentExpiresAt)
   Event.remove({ createdAt: { $lt: date } }, done)
   console.log('Agenda "delete old events" complited: ', date.toLocaleTimeString())
+  console.log('Payment expires: ', paymentExpiresAt)
 })
 
 export default agenda;
