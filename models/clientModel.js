deac7db2718a994bf0591327fe9ca3d54a1cf88c
Ilya Kulgavy <i.kulgavy@gmail.com>
2017-10-02 15:04:35 +0300

    client fixes: unique

--- models/clientModel.js
+++ models/clientModel.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema
 
 const ClientSchema = new Schema({
   name: { type: String, required: true },
-  phoneNumber: { type: String, required: true },
+  phoneNumber: { type: String, required: true, unique: true },
   comments: { type: [], default: [] },
   email: { type: String },
   social: { type: String }
