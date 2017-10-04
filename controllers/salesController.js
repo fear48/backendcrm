2309348b2b8a9a58c7e8c8e733946496ba6002d5
Ilya Kulgavy <i.kulgavy@gmail.com>
2017-10-04 20:22:33 +0300

    C:/Program Files/Git/api/sales/checkdates added

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -1,44 +1,51 @@
-import Sale from '../models/salesModel';
-import Room from '../models/roomModel';
+import Sale from "../models/salesModel";
+import Room from "../models/roomModel";
 
 export default {
   getAllSales: (req, res, next) => {
-    Sale.find({}).then(response => {
-      res.send(response)
-    }).catch(err => {
-      next({ status: 403, message: err.message })
-    })
+    Sale.find({})
+      .then(response => {
+        res.send(response);
+      })
+      .catch(err => {
+        next({ status: 403, message: err.message });
+      });
   },
   addNewSale: (req, res, next) => {
     const { roomId } = req.body;
-    Room.findOne({ _id: roomId }).then(({ roomName }) => {
-
-      Sale({ ...req.body, roomName }).save().then(response => {
-        res.send(response)
+    Room.findOne({ _id: roomId })
+      .then(({ roomName }) => {
+        Sale({ ...req.body, roomName })
+          .save()
+          .then(response => {
+            res.send(response);
+          });
       })
-
-    }).catch(err => {
-      next({ status: 403, message: err.message })
-    })
+      .catch(err => {
+        next({ status: 403, message: err.message });
+      });
   },
   changeSaleInfo: (req, res, next) => {
     const { id } = req.params;
     if (req.body.roomId) {
-      Room.findOne({ _id: req.body.roomId }).then(({ roomName }) =>
-        Sale.findOneAndUpdate({ _id: id }, { ...req.body, roomName })
-      ).then(response => {
-        res.send(response)
-      })
-        .catch(err => {
-          next({ status: 403, message: err.message })
+      Room.findOne({ _id: req.body.roomId })
+        .then(({ roomName }) =>
+          Sale.findOneAndUpdate({ _id: id }, { ...req.body, roomName })
+        )
+        .then(response => {
+          res.send(response);
         })
+        .catch(err => {
+          next({ status: 403, message: err.message });
+        });
     } else {
-      Sale.findOneAndUpdate({ _id: id }, req.body).then(response => {
-        res.send(response)
-      })
-        .catch(err => {
-          next({ status: 403, message: err.message })
+      Sale.findOneAndUpdate({ _id: id }, req.body)
+        .then(response => {
+          res.send(response);
         })
+        .catch(err => {
+          next({ status: 403, message: err.message });
+        });
     }
   },
   deleteSale: (req, res, next) => {
@@ -46,9 +53,24 @@ export default {
     Sale.findByIdAndRemove(id)
       .then(() => Sale.find({}))
       .then(response => {
-        res.send(response)
-      }).catch(err => {
-        next({ status: 403, message: err.message })
+        res.send(response);
       })
+      .catch(err => {
+        next({ status: 403, message: err.message });
+      });
+  },
+  cheakDates: (req, res, next) => {
+    const { startDate, endDate } = req.body;
+    if (startDate < endDate) {
+      Sale.find({ endDate: { $gte: startDate }, startDate: { $lte: endDate } })
+        .then(response => {
+          res.send(response[0]);
+        })
+        .catch(err => {
+          next({ status: 403, message: err.message });
+        });
+    } else {
+      next({ status: 403, message: "Wrong dates" });
+    }
   }
-}
+};
--- routers/sales.js
+++ routers/sales.js
@@ -7,5 +7,6 @@ router.get("/", salesController.getAllSales);
 router.post("/", salesController.addNewSale);
 router.put("/:id", salesController.changeSaleInfo);
 router.delete("/:id", salesController.deleteSale);
+router.post("/checkdates", salesController.cheakDates);
 
 export default router;
